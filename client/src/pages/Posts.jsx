0b786eded2a62b45349d3584dd0d17bfca26cadb
iamwesofph <piuswesley@gmail.com>
2023-10-30 20:46:24 +0800

    Create post component

--- client/src/pages/Posts.jsx
+++ client/src/pages/Posts.jsx
@@ -1,7 +1,10 @@
 import { useState } from "react";
 import PlusIcon from "../icons/plus.svg?react";
 import PlusCircle from "../icons/pluscircle.svg?react";
-import Modal from "./ModalNewPost";
+import ModalNewPost from "./ModalNewPost";
+import DotDotDot from "../icons/dotdotdot.svg?react";
+import Like from "../icons/like.svg?react";
+import Comment from "../icons/comment.svg?react";
 
 const Posts = ({ user }) => {
     const [showNewPost, setShowNewPost] = useState(false);
@@ -13,7 +16,7 @@ const Posts = ({ user }) => {
 
     return (
         <>
-            {showNewPost && <Modal setShowNewPost={setShowNewPost} user={user} />}
+            {showNewPost && <ModalNewPost setShowNewPost={setShowNewPost} user={user} />}
 
             <div className="flex flex-col p-6 text-black text-sm">
                 <div className="flex items-center mx-auto w-80 h-16 border ring-1 mb-4 rounded-sm bg-slate-200 p-2 hover:cursor-pointer hover:bg-cyan-400 transition-colors" onClick={handleNewPost}>
@@ -24,7 +27,35 @@ const Posts = ({ user }) => {
                     </div>
                 </div>
 
-                <div className="flex flex-col mx-auto w-80 border ring-1 mb-4 rounded-sm bg-slate-200 p-2">...</div>
+                <div className="flex flex-col mx-auto w-80 border ring-1 mb-4 rounded-sm bg-slate-200 p-2">
+                    <div className="flex items-center justify-between mb-2">
+                        <div className="flex">
+                            <img className="rounded-full h-10 w-10 mr-2 object-cover border border-white" src={user.profilePhoto || noProfilePhoto} alt="profile photo" referrerPolicy="no-referrer" />
+                            <div className="flex flex-col justify-center">
+                                <span className="font-bold">{user.firstName || user.displayName}</span>
+                                <span className="text-xs">October 27, 2023</span>
+                            </div>
+                        </div>
+                        <div>
+                            <DotDotDot className="w-4"></DotDotDot>
+                        </div>
+                    </div>
+
+                    <hr className="w-full border-t border-gray-300 mb-4" />
+
+                    <hr className="w-full border-t border-gray-300 mb-4" />
+
+                    <div className="flex justify-around">
+                        <div className="flex items-center cursor-pointer">
+                            <Like className="w-4 mr-2"></Like>
+                            <span>Like</span>
+                        </div>
+                        <div className="flex items-center cursor-pointer">
+                            <Comment className="w-4 mr-2"></Comment>
+                            <span>Comment</span>
+                        </div>
+                    </div>
+                </div>
             </div>
         </>
     );
