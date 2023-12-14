a1b2716e47793752b2475ec71f663ea4d11a992e
iamwesofph <piuswesley@gmail.com>
2023-12-14 11:59:08 +0800

    Change bottom navbar

--- client/src/pages/BottomNavbar.jsx
+++ client/src/pages/BottomNavbar.jsx
@@ -13,6 +13,13 @@ const BottomNavbar = ({ user }) => {
             <NavLink className="flex justify-center relative grow py-3 rounded-t-lg" to="/">
                 <HomeIcon className="fill-current" />
             </NavLink>
+            <NavLink className="flex justify-center relative grow py-3 rounded-t-lg" to={"/friends-page/suggestions"} style={{ textDecoration: "none" }}>
+                <FriendsIcon className="fill-current" />
+            </NavLink>
+            <NavLink className="group relative flex grow items-center justify-center px-8 py-2 hover:bg-light-c dark:hover:bg-dark-a transition-colors rounded-t-lg" to="/messenger">
+                <MessengerIcon className="fill-current w-6 h-6" />
+                <div className="absolute top-14 left-0 right-0 mx-auto w-fit invisible group-hover:visible delay-200 opacity-80 bg-white rounded-md text-sm px-2 py-1 text-black shadow-md">Messenger</div>
+            </NavLink>
             {isUserLoaded && (
                 <>
                     <NavLink className="flex justify-center relative grow py-3 rounded-t-lg" to={`/profile/${user.id}`}>
@@ -20,14 +27,8 @@ const BottomNavbar = ({ user }) => {
                     </NavLink>
                 </>
             )}
-            <NavLink className="flex justify-center relative grow py-3 rounded-t-lg" to={"/friends-page/suggestions"} style={{ textDecoration: "none" }}>
-                <FriendsIcon className="fill-current" />
-            </NavLink>
             <NavLink className="flex justify-center relative grow py-3 rounded-t-lg" to="/play">
                 <PlayIcon className="fill-current" />
-            </NavLink>
-            <NavLink className="flex justify-center relative grow py-3 rounded-t-lg" to="/leaderboard">
-                <LeaderboardIcon className="fill-current" />
             </NavLink>
         </div>
     );
